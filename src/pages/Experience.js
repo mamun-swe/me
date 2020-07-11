7ce1d5092a030436c739ad6fe32450aebc52c7ff
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-07-12 03:01:00 +0600

    okk

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './style.css';
 
 const Experience = () => {
     return (
-        <div>
-            <h1>Experience</h1>
+        <div className="experience about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-right">
+                        <h1 className="text-uppercase name">job</h1>
+                        <h1 className="text-uppercase title">experience</h1>
+                        <br />
+                        <div className="jobs">
+
+                            {/* Job */}
+                            <div className="job mb-5">
+                                <h3 className="mb-0 text-capitalize">dhaka boss ( PVT ) Ltd.</h3>
+                                <h4 className="text-capitalize mb-0">software engineer ( back-end )</h4>
+                                <p>10 MAY, 2020 - Present</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './style.css';
 
 const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>
+        <div className="projects about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-right">
+                        <h1 className="text-uppercase name">work</h1>
+                        <h1 className="text-uppercase title">projects</h1>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
